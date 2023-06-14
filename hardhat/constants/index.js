15e308b3efc5df80b28f5407cf0cb663d74fb175
Guilherme Duccini <guilhermeduccini@gmail.com>
2023-06-14 20:38:18 -0300

    Update metadata URL

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -2,6 +2,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xe1D82dc581690F4e7F1B194195411e1C1dD6Ab2e";
 
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "";
+const METADATA_URL = "https://nft-collection-ochre-xi.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
